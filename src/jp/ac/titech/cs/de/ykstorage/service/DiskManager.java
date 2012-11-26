a52685301fc676afad891cd0b22f5cd83a0e433e
09oguri <oguri@de.cs.titech.ac.jp>
2012-11-26 13:29:37 +0900

    our prototype of storage system

--- src/jp/ac/titech/cs/de/ykstorage/service/DiskManager.java
+++ src/jp/ac/titech/cs/de/ykstorage/service/DiskManager.java
@@ -1,5 +1,111 @@
 package jp.ac.titech.cs.de.ykstorage.service;
 
-public class DiskManager {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.HashMap;
 
+
+public class DiskManager {
+	private static final String[] diskpaths = {"/disk0/", "/disk1/"};	// TODO
+	
+	private HashMap<String, String> keyFileMap = new HashMap<String, String>();
+	private int diskIndex = 0;	// ラウンドロビンでディスクの選択時に使用
+	
+	
+	public byte[] get(String key) {
+		return isNullOrEmpty(key) ? null: read(key); 
+	}
+	
+	public boolean put(String key, byte[] value) {
+		return isNullOrEmpty(key) ? false: write(key, value);
+	}
+	
+	public boolean delete(String key) {
+		return isNullOrEmpty(key) ? false: remove(key);
+	}
+	
+	private static boolean isNullOrEmpty(String key) {
+		if(key == null || key.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	// キーに基づいて格納先のディスクを選択
+	private String selectDisk(String key) {
+		String filepath = keyFileMap.get(key);
+		if(filepath != null) {
+			return filepath;
+		}
+		
+		filepath = roundRobin(key);
+		keyFileMap.put(key, filepath);
+		return filepath;
+	}
+	
+	private String roundRobin(String key) {
+		String filepath = diskpaths[diskIndex] + key;
+		diskIndex++;
+		if(diskIndex > diskpaths.length - 1) {
+			diskIndex = 0;
+		}
+		return filepath;
+	}
+	
+	private byte[] read(String key) {
+		String filepath = keyFileMap.get(key);
+		try {
+			File f = new File(filepath);
+			FileInputStream fis = new FileInputStream(f);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			
+			byte[] value = new byte[(int) f.length()];	// TODO
+			bis.read(value);
+			
+			bis.close();
+			return value;
+		}catch(NullPointerException e) {
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private boolean write(String key, byte[] value) {
+		String filepath = selectDisk(key);
+		try {
+			File f = new File(filepath);
+			FileOutputStream fos = new FileOutputStream(f);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			
+			bos.write(value);	// TODO
+			bos.flush();
+			
+			bos.close();
+			return true;
+		}catch(Exception e) {
+			keyFileMap.remove(key);
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private boolean remove(String key) {
+		String filepath = keyFileMap.get(key);
+		keyFileMap.remove(key);
+		try {
+			File f = new File(filepath);
+			return f.delete();
+		}catch(NullPointerException e) {
+			
+		}catch(Exception e) {
+			keyFileMap.put(key, filepath);
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
---
+++ test/test/jp/ac/titech/cs/de/ykstorage/service/DiskManagerTest.java
@@ -0,0 +1,85 @@
+package test.jp.ac.titech.cs.de.ykstorage.service;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import jp.ac.titech.cs.de.ykstorage.service.DiskManager;
+
+
+@RunWith(JUnit4.class)
+public class DiskManagerTest {
+	
+	private String key = "key";
+	private String key2 = "key2";
+	private String key3 = "key3";
+	private String nullKey = null;
+	private String emptyKey = "";
+	private byte[] value = "value".getBytes();
+	private byte[] value2 = "value2".getBytes();
+	private byte[] value3 = "value3".getBytes();
+	private DiskManager dm = new DiskManager();
+	
+	
+	@Test
+	public void testGet() {
+		assertThat(dm.get(key), is(nullValue()));
+		assertThat(dm.get(nullKey), is(nullValue()));
+		assertThat(dm.get(emptyKey), is(nullValue()));
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPut() {
+		assertThat(dm.put(key, value), is(true));
+		assertThat(dm.put(nullKey, value), is(false));
+		assertThat(dm.put(emptyKey, value), is(false));
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDelete() {
+		assertThat(dm.delete(key), is(false));
+		assertThat(dm.delete(nullKey), is(false));
+		assertThat(dm.delete(emptyKey), is(false));
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void mainTest() {
+		DiskManager diskMgr = new DiskManager();
+		assertThat(diskMgr.put(key, value), is(true));
+		assertThat(diskMgr.get(key), is(value));
+		assertThat(diskMgr.delete(key), is(true));
+	}
+	
+	@Test
+	public void mainTest2() {
+		DiskManager diskMgr = new DiskManager();
+		assertThat(diskMgr.put(key, value), is(true));
+		assertThat(diskMgr.put(key2, value2), is(true));
+		assertThat(diskMgr.get(key), is(value));
+		assertThat(diskMgr.get(key2), is(value2));
+		
+		assertThat(diskMgr.put(key, value2), is(true));
+		assertThat(diskMgr.get(key), is(value2));
+		assertThat(diskMgr.put(key2, value), is(true));
+		assertThat(diskMgr.get(key2), is(value));
+		
+		assertThat(diskMgr.put(key3, value3), is(true));
+		assertThat(diskMgr.get(key3), is(value3));
+		
+		assertThat(diskMgr.delete(key), is(true));
+		assertThat(diskMgr.delete(key2), is(true));
+		assertThat(diskMgr.delete(key3), is(true));
+		
+		assertThat(diskMgr.get(key), is(nullValue()));
+		assertThat(diskMgr.put(key, value), is(true));
+		assertThat(diskMgr.get(key), is(value));
+		assertThat(diskMgr.delete(key), is(true));
+	}
+
+}
