994b20abac93641cf085ec954841e0fca213b468
Satoshi Hikida <s.hikida@hotmail.co.jp>
2012-12-04 18:02:18 +0900

    aggregate the tests into a suite

---
+++ test/test/jp/ac/titech/cs/de/ykstorage/service/_StorageTestSuite.java
@@ -0,0 +1,17 @@
+package test.jp.ac.titech.cs.de.ykstorage.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import test.jp.ac.titech.cs.de.ykstorage.service.cmm.CacheMemoryManagerTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	DiskManagerTest.class,
+	StorageManagerTest.class,
+	StorageServiceTest.class,
+	ValueTest.class,
+	CacheMemoryManagerTest.class
+})
+
+public class _StorageTestSuite {}
