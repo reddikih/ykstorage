1e1bcb576e83d8720de5364f0835f958df265fdd
Satoshi Hikida <s.hikida@hotmail.co.jp>
2012-12-04 15:11:26 +0900

    add Value object.
    It is used with null object pattern.

---
+++ test/test/jp/ac/titech/cs/de/ykstorage/service/Value.java
@@ -0,0 +1,18 @@
+package test.jp.ac.titech.cs.de.ykstorage.service;
+
+public class Value {
+	
+	private byte[] value;
+	
+	// Null Object pattern
+	public static final Value NULL = new Value(new byte[0]);
+	
+	public Value(byte[] value) {
+		this.value = value;
+	}
+	
+	public byte[] getValue() {
+		return value;
+	}
+
+}
