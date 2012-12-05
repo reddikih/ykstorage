e2feb6a38e87b1f6dfe459e2e564a6940c5a74ac
09oguri <oguri@de.cs.titech.ac.jp>
2012-12-05 14:01:21 +0900

    implement state manager.

---
+++ test/test/jp/ac/titech/cs/de/ykstorage/service/StateManagerTest.java
@@ -0,0 +1,48 @@
+package test.jp.ac.titech.cs.de.ykstorage.service;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import jp.ac.titech.cs.de.ykstorage.service.Parameter;
+import jp.ac.titech.cs.de.ykstorage.service.StateManager;
+
+
+@RunWith(JUnit4.class)
+public class StateManagerTest {
+	
+	@Test
+	public void startTest() {
+		StateManager sm = new StateManager(Parameter.NUMBER_OF_DATADISK, Parameter.SPIN_DOWN_THRESHOLD);
+		sm.start();
+		assertThat(sm.getDiskState(0), is(StateManager.ACTIVE));
+	}
+	
+	@Test
+	public void spindownTest() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void mainTest() {
+		StateManager sm = new StateManager(Parameter.NUMBER_OF_DATADISK, Parameter.SPIN_DOWN_THRESHOLD);
+		
+		assertThat(sm.setDiskState(0, StateManager.ACTIVE), is(true));
+		assertThat(sm.setDiskState(1, StateManager.IDLE), is(true));
+		assertThat(sm.setDiskState(2, StateManager.STANDBY), is(true));
+		
+		assertThat(sm.getDiskState(0), is(StateManager.ACTIVE));
+		assertThat(sm.getDiskState(1), is(StateManager.IDLE));
+		assertThat(sm.getDiskState(2), is(StateManager.STANDBY));
+		
+		assertThat(sm.setIdleIntime(0, System.currentTimeMillis()), is(true));
+		assertThat(sm.setIdleIntime(1, System.currentTimeMillis()), is(true));
+		assertThat(sm.setIdleIntime(2, System.currentTimeMillis()), is(true));
+		
+		assertThat(sm.setDiskState(0, StateManager.IDLE), is(true));
+		assertThat(sm.getDiskState(0), is(StateManager.IDLE));
+	}
+}
