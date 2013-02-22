242b6a0addc53647843cf86036948ec473093f5f
09oguri <oguri@de.cs.titech.ac.jp>
2013-02-22 14:15:59 +0900

    refactor CacheMemoryManagerTest.

--- test/test/jp/ac/titech/cs/de/ykstorage/service/cmm/CacheMemoryManagerTest.java
+++ test/test/jp/ac/titech/cs/de/ykstorage/service/cmm/CacheMemoryManagerTest.java
@@ -10,44 +10,132 @@ import java.util.Set;
 import jp.ac.titech.cs.de.ykstorage.service.Value;
 import jp.ac.titech.cs.de.ykstorage.service.cmm.CacheMemoryManager;
 
+import org.junit.runner.RunWith;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 
+@RunWith(Enclosed.class)
 public class CacheMemoryManagerTest {
 
+	static int key1 = 1;
+	static Value value1 = new Value(new byte[]{1,2,3});
+	static int key2 = 2;
+	static Value value2 = new Value(new byte[]{4,5,6});
+	static int key3 = 3;
+	static Value value3 = new Value(new byte[]{7,8,9});
+	static int key4 = 4;
+	static Value value4 = new Value(new byte[]{10,11,12});
+	
+	
+	public static class InitializeThreshold {
+		CacheMemoryManager cmm;
+		
+		@Test(expected=IllegalArgumentException.class)
+		public void thresholdLessThan0() throws Exception {
+			cmm = new CacheMemoryManager(10, -0.1);
+		}
+		
+		@Test(expected=IllegalArgumentException.class)
+		public void thresholdMoreThan1() throws Exception {
+			cmm = new CacheMemoryManager(10, 1.1);
+		}
+	}
+	
+	public static class InitializeMemorySize {
+		CacheMemoryManager cmm;
+		
+		@Test
+		public void memory0Byte() {
+			cmm = new CacheMemoryManager(0, 1.0);
+			Value actual = cmm.put(key1, value1);
+			assertThat(actual, is(Value.NULL));
+		}
+		
+		@Test(expected=IllegalArgumentException.class)
+		public void memoryIntegerMaxByte() {
+			cmm = new CacheMemoryManager(Integer.MAX_VALUE + 1, 1.0);
+		}
+		
+		@Test
+		public void memory10Byte() {
+			cmm = new CacheMemoryManager(10, 1.0);
+			Value actual = cmm.put(key1, value1);
+			assertThat(actual, is(value1));
+		}
+	}
+	
+	public static class InvalidKey {
+		CacheMemoryManager cmm;
+		
+		@Before
+		public void setUp() throws Exception {
+			cmm = new CacheMemoryManager(10, 1.0);
+			cmm.put(key1, value1);
+		}
+		
+		@Test
+		public void getNullObjectByInvalidKey() {
+			Value actual = cmm.get(key2);
+			assertThat(actual, is(Value.NULL));
+		}
+	}
+	
+	public static class IdempotentPut {
+		CacheMemoryManager cmm;
+		
+		@Before
+		public void setUp() throws Exception {
+			cmm = new CacheMemoryManager(10, 1.0);
+			cmm.put(key1, value1);
+		}
+		
+		@Test
+		public void Idempotent() {
+			Value actual = cmm.get(key1);
+			assertThat(actual, is(value1));
+			
+			actual = cmm.get(key1);
+			assertThat(cmm.get(key1), is(value1));
+			
+			actual = cmm.get(key1);
+			assertThat(cmm.get(key1), is(value1));
+		}
+	}
+	
+	public static class PutAndGet {
+		CacheMemoryManager cmm;
+		
+		@Before
+		public void setUp() throws Exception {
+			cmm = new CacheMemoryManager(10, 1.0);
+			cmm.put(key1, value1);
+			cmm.put(key2, value2);
+			cmm.put(key3, value3);
+		}
+		
+		@Test
+		public void getTest() {
+			Value actual = cmm.get(key1);
+			assertThat(actual, is(value1));
+			
+			actual = cmm.get(key2);
+			assertThat(actual, is(value2));
+			
+			actual = cmm.get(key3);
+			assertThat(actual, is(value3));
+			
+			actual = cmm.get(key1);
+			assertThat(actual, is(value1));
+		}
+	}
+	
 	private CacheMemoryManager cmm;
 
 	@Before
 	public void setUpClass() {
 		//TODO we can use parameterized tests.
 		this.cmm = new CacheMemoryManager(10, 1.0);
-	}
-
-	@Test(expected=IllegalArgumentException.class)
-	public void initializeArgumentTest() {
-		new CacheMemoryManager(10, 10);
-		new CacheMemoryManager(10, -1.0);
-		new CacheMemoryManager(10, 1.1);
-	}
-
-	@Test
-	public void testPutAndGet() {
-		int key1 = 1;
-		Value value1 = new Value(new byte[]{1,2,3});
-		assertThat(cmm.put(key1, value1), is(value1));
-
-		int key2 = 2;
-		Value value2 = new Value(new byte[]{4,5,6,7,8,9,10});
-		assertThat(cmm.put(key2, value2), is(value2));
-
-		int key3 = 3;
-		Value value3 = new Value(new byte[]{11});
-		assertThat(cmm.put(key3, value3), is(Value.NULL));
-
-		assertThat(cmm.get(key1), is(value1));
-		assertThat(cmm.get(key2), is(value2));
-		assertThat(cmm.get(key1), is(value1));
-		assertThat(cmm.get(key1), not(value2));
 	}
 
 	@Test
@@ -81,27 +169,6 @@ public class CacheMemoryManagerTest {
 		// also key1 and key2 are available
 		assertThat(cmm.get(key1), is(value1));
 		assertThat(cmm.get(key2), is(value2));
-	}
-
-	@Test
-	public void getNullObjectByInvalidKey() {
-		int key = 1;
-		Value value = new Value(new byte[]{1,2,3});
-		cmm.put(key, value);
-		assertThat(cmm.get(key), is(value));
-
-		int invalidKey = 2;// invalid key id
-		assertThat(cmm.get(invalidKey), is(Value.NULL));
-	}
-
-	@Test
-	public void idempotentPut() {
-		int key = 1;
-		Value value = new Value(new byte[]{1,2,3});
-		cmm.put(key, value);
-		assertThat(cmm.get(key), is(value));
-		assertThat(cmm.get(key), is(value));
-		assertThat(cmm.get(key), is(value));
 	}
 
 	@Test
