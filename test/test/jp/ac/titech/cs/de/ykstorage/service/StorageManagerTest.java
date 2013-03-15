1ffe9d4e53fb0666da0ba6633e90c791c88ae02f
09oguri <oguri@de.cs.titech.ac.jp>
2013-03-15 15:04:23 +0900

    modify StorageManagerTest.

--- test/test/jp/ac/titech/cs/de/ykstorage/service/StorageManagerTest.java
+++ test/test/jp/ac/titech/cs/de/ykstorage/service/StorageManagerTest.java
@@ -24,6 +24,12 @@ public class StorageManagerTest {
 	static byte[] value3 = new byte[]{7,8,9,10,11,12};
 	static String key4 = "key4";
 	static byte[] value4 = new byte[]{1,2,3,4,5,6,7,8,9,10,11,12};
+	static String key5 = "key5";
+	static byte[] value5 = new byte[]{1,2,3,4,5};
+	static String key6 = "key6";
+	static byte[] value6 = new byte[]{6,7,8,9,10};
+	static String key7 = "key7";
+	static byte[] value7 = new byte[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14};
 	
 	public static class Memory0Byte {
 		StorageManager sm;
@@ -64,7 +70,7 @@ public class StorageManagerTest {
 		@Before
 		public void setUp() {
 			long cmmMax = 10L;
-			double threshold = 0.0;
+			double threshold = 1.0;
 			CacheMemoryManager cmm = new CacheMemoryManager(cmmMax, threshold);
 			
 			DiskManager dm = new DiskManager(
@@ -117,6 +123,22 @@ public class StorageManagerTest {
 		}
 		
 		@Test
+		public void lruOnMemoryTest2() {
+			sm.put(key5, value5);
+			sm.put(key6, value6);
+			sm.put(key3, value3);
+			
+			byte[] actual = sm.get(key5);
+			assertThat(actual, is(value5));
+			
+			actual = sm.get(key6);
+			assertThat(actual, is(value6));
+			
+			actual = sm.get(key3);
+			assertThat(actual, is(value3));
+		}
+		
+		@Test
 		public void largeSizePutTest() {
 			boolean actual = sm.put(key4, value4);
 			assertThat(actual, is(true));
@@ -128,6 +150,24 @@ public class StorageManagerTest {
 			
 			byte[] actual = sm.get(key4);
 			assertThat(actual, is(value4));
+		}
+		
+		@Test
+		public void updateTheSameKeys() {
+			sm.put(key1, value1);
+			sm.put(key1, value2);
+			
+			byte[] actual = sm.get(key1);
+			assertThat(actual, is(value2));
+		}
+		
+		@Test
+		public void updateTheSameKeys2() {
+			sm.put(key4, value4);
+			sm.put(key4, value7);
+			
+			byte[] actual = sm.get(key4);
+			assertThat(actual, is(value7));
 		}
 	}
 	
