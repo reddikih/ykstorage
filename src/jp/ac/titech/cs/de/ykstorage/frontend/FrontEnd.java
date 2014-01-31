519e75d7ca0e89ce70359fbac77f825cf75ddfa4
hikida satoshi <hikida@de.cs.titech.ac.jp>
2014-01-31 13:34:23 +0900

    remove unnecessary variable.

---
+++ src/jp/ac/titech/cs/de/ykstorage/frontend/FrontEnd.java
@@ -0,0 +1,235 @@
+package jp.ac.titech.cs.de.ykstorage.frontend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class FrontEnd {
+
+    private static Logger logger = LoggerFactory.getLogger(FrontEnd.class);
+
+    private final int NUM_THREADS;
+    private final Executor service;
+    private final ServerSocket socket;
+
+    public static FrontEnd getInstance(int threads, int port) throws IOException {
+        return new FrontEnd(threads, port);
+    }
+
+    private FrontEnd(int threads, int port) throws IOException {
+        this.NUM_THREADS = threads;
+        this.service = Executors.newFixedThreadPool(this.NUM_THREADS);
+        this.socket = new ServerSocket(port);
+        logger.info("Host:{} Port:{} ThreadPool:{}",
+                socket.getInetAddress().toString(),
+                socket.getLocalPort(),
+                threads);
+    }
+
+    public void start() {
+        if (this.socket == null)
+            throw new IllegalStateException("Socket is not opend.");
+
+        logger.info("into the while loop");
+        while (true) {
+            try {
+                final Socket conn = this.socket.accept();
+                Runnable task = getTask(conn);
+                service.execute(task);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Runnable getTask(Socket conn) throws IOException {
+        Runnable result = null;
+
+        ClientRequest request = parseRequest(conn);
+        if (request == null)
+            throw new IllegalStateException("couldn't parse client request.");
+
+        if (request.getCommand().equals(RequestCommand.READ))
+            result = new ReadTask(conn, request);
+        else if (request.getCommand().equals(RequestCommand.WRITE))
+            result = new WriteTask(conn, request);
+        else
+            throw new IllegalStateException("request command is invalid.: " + request.getCommand());
+
+        return result;
+    }
+
+    private ClientRequest parseRequest(Socket conn) {
+        ClientRequest result = null;
+        RequestHeader header = new RequestHeader(conn);
+
+        if (header.getCommand().equals(RequestCommand.READ) ||
+            header.getCommand().equals(RequestCommand.DELETE))
+            return new ClientRequest(header, null);
+
+        // extract payload for write request
+        try {
+            InputStream in = conn.getInputStream();
+
+            int length = header.getLength();
+            byte[] payload = new byte[length];
+            int readBytes = in.read(payload);
+            if (readBytes != payload.length)
+                throw new IOException("request payload is incorrected.");
+
+            result = new ClientRequest(header, payload);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private class WriteTask implements Runnable {
+        private final Socket conn;
+        private final ClientRequest request;
+
+        protected WriteTask(Socket conn, ClientRequest request) {
+            this.conn = conn;
+            this.request = request;
+        }
+        @Override
+        public void run() {
+            logger.info("starting write request Request Key:{}", request.getKey());
+
+            try {
+                OutputStream out = conn.getOutputStream();
+                byte[] payload = this.request.getPayload();
+                byte[] response;
+                if (payload != null) {
+                    response = new byte[]{new Byte("200"), 0x00,0x00,0x00,0x00};
+                } else {
+                    response = new byte[]{new Byte("500"), 0x00,0x00,0x00,0x00};
+                }
+                out.write(response);
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ReadTask implements Runnable {
+        private final Socket conn;
+        private final ClientRequest request;
+
+        protected ReadTask(Socket conn, ClientRequest request) {
+            this.conn = conn;
+            this.request = request;
+        }
+        @Override
+        public void run() {
+
+        }
+    }
+
+    public enum RequestCommand {
+        READ,
+        WRITE,
+        DELETE,
+    }
+
+    private class RequestHeader {
+        private RequestCommand command;
+        private long key;
+        private int length;
+
+        RequestHeader(Socket sock) {
+            parseHeader(sock);
+        }
+
+        public RequestCommand getCommand() {return this.command;}
+        public long getKey() {return this.key;}
+        public int getLength() {return this.length;}
+
+        private void parseHeader(Socket sock) {
+            try {
+                InputStream in = sock.getInputStream();
+
+                byte[] command = new byte[2];
+                int readByte = in.read(command);
+                if (readByte != command.length)
+                    throw new IOException("couldn't read request command");
+
+                if (command[0] == 0x00) {
+                    if (command[1] == 0x01) this.command = RequestCommand.READ;
+                    if (command[1] == 0x10) this.command = RequestCommand.WRITE;
+                } else if (command[0] == 0x01) {
+                    if (command[1] == 0x00) this.command = RequestCommand.DELETE;
+                } else {
+                    throw new IllegalStateException("request command is invalid.: " + ((int)command[0] + (int)command[1]));
+                }
+
+                logger.debug("Requested Command: {}", this.command);
+
+                // get key(8bytes)
+                long key = 0L;
+                int k = 64, offset = 8;
+                byte[] keyVal = new byte[8];
+//                readByte = in.read(keyVal, command.length, keyVal.length);
+                readByte = in.read(keyVal);
+                if (readByte != keyVal.length)
+                    throw new IOException("couldn't read request command");
+
+                for (int i = 0; i < keyVal.length; i++)
+                    key |= ((long)keyVal[i] << (k -= offset));
+                this.key = key;
+
+                logger.debug("Request Key: {}", this.key);
+
+                //get length(4bytes)
+                int length = 0;
+                k = 32;
+                byte[] lengthVal = new byte[4];
+//                readByte = in.read(lengthVal, readByte, lengthVal.length);
+                readByte = in.read(lengthVal);
+                if (readByte != lengthVal.length)
+                    throw new IOException("couldn't read request command");
+
+                for (int i = 0; i < lengthVal.length; i++)
+                    this.length |= ((long)lengthVal[i] << (k -= offset));
+
+                logger.debug("Request Length: {}", this.length);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ClientRequest {
+        private final RequestHeader header;
+        private byte[] payload;
+
+        protected ClientRequest(RequestHeader header, byte[] payload) {
+            this.header = header;
+            this.payload = payload;
+        }
+
+        public RequestCommand getCommand() { return this.header.getCommand(); }
+        public long getKey() { return this.header.getKey(); }
+        public int getLength() { return this.header.getLength(); }
+        public byte[] getPayload() { return this.payload; }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int thread = 1;
+        int port = 9999;
+        FrontEnd server = FrontEnd.getInstance(1, 9999);
+        logger.info("Starting FrontEnd");
+        server.start();
+    }
+}
---
+++ test/test/jp/ac/titech/cs/de/ykstorage/frontend/FrontEndTest.java
@@ -0,0 +1,106 @@
+package test.jp.ac.titech.cs.de.ykstorage.frontend;
+
+import jp.ac.titech.cs.de.ykstorage.frontend.FrontEnd;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+@RunWith(JUnit4.class)
+public class FrontEndTest {
+
+    private int thread = 1;
+    private int port = 9999;
+    private String hostName = "localhost";
+
+//    @Test(timeout=10000)
+    @Test
+    public void writeToFrontEnd() {
+        long id = 223L;
+        int size = 8;
+        byte[] req = createWriteRequest(id, size);
+        try {
+            Socket conn = new Socket(this.hostName, this.port);
+            InputStream in = conn.getInputStream();
+            OutputStream out = conn.getOutputStream();
+
+            out.write(req);
+            out.flush();
+
+            int response = in.read();
+            assertThat(response, is(200));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("IOException occurred.");
+        }
+    }
+
+//    @Test
+//    public void readFromFrontEnd() {
+//        try {
+//            FrontEnd frontend = FrontEnd.getInstance(thread, port);
+//
+//            Socket conn = new Socket(hostName, port);
+//            InputStream in = conn.getInputStream();
+//            OutputStream out = conn.getOutputStream();
+//
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            fail("IOException occurred.");
+//        }
+//    }
+
+    private byte[] createReadRequest(long id) {
+        byte[] header = {
+                0x00, 0x01,
+                (byte)(0x00000000000000ff & (id >>> 56)) ,
+                (byte)(0x00000000000000ff & (id >>> 48)) ,
+                (byte)(0x00000000000000ff & (id >>> 40)) ,
+                (byte)(0x00000000000000ff & (id >>> 32)) ,
+                (byte)(0x00000000000000ff & (id >>> 24)) ,
+                (byte)(0x00000000000000ff & (id >>> 16)) ,
+                (byte)(0x00000000000000ff & (id >>> 8)) ,
+                (byte)(0x00000000000000ff & (id))
+        };
+        return header;
+    }
+
+    private byte[] createWriteRequest(long id, int size) {
+        byte[] header = {
+                0x00, 0x10,
+                (byte)(0x00000000000000ff & (id >>> 56)) ,
+                (byte)(0x00000000000000ff & (id >>> 48)) ,
+                (byte)(0x00000000000000ff & (id >>> 40)) ,
+                (byte)(0x00000000000000ff & (id >>> 32)) ,
+                (byte)(0x00000000000000ff & (id >>> 24)) ,
+                (byte)(0x00000000000000ff & (id >>> 16)) ,
+                (byte)(0x00000000000000ff & (id >>> 8)) ,
+                (byte)(0x00000000000000ff & id),
+                (byte)(0x000000ff & (size >>> 24)),
+                (byte)(0x000000ff & (size >>> 16)),
+                (byte)(0x000000ff & (size >>> 8)),
+                (byte)(0x000000ff & size),
+        };
+        byte[] request = Arrays.copyOf(header, header.length + size);
+        for (int i = header.length; i < request.length; i++) {
+            request[i] = 0x62;
+        }
+        return request;
+    }
+}
