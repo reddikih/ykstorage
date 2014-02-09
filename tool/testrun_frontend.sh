fc50922028f33508a2b1b8137b8f0141ec9567fd
hikida.satoshi <s.hikida@hotmail.co.jp>
2014-02-09 19:16:10 +0900

    update test script which runs FronEnd

--- tool/testrun_frontend.sh
+++ tool/testrun_frontend.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-IP_ADDR=
-PORT=
+JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 echo "YKSTORAGE_HOME = " $YKSTORAGE_HOME
 
@@ -9,4 +8,20 @@ if [ -f ${YKSTORAGE_HOME}/bin/logback.xml ];then
     mv ${YKSTORAGE_HOME}/bin/logback.xml ${YKSTORAGE_HOME}/bin/_logback.xml
 fi
 
-java -cp ${YKSTORAGE_HOME}/bin:${YKSTORAGE_HOME}/lib/junit4.10.jar:${YKSTORAGE_HOME}/lib/logback-classic-1.0.9.jar:${YKSTORAGE_HOME}/lib/logback-core-1.0.9.jar:${YKSTORAGE_HOME}/lib/slf4j-api-1.7.2.jar jp.ac.titech.cs.de.ykstorage.frontend.FrontEnd
+CYGWIN=false
+case "$(uname)" in
+CYGWIN*) CYGWIN=true;;
+esac
+
+if $CYGWIN; then
+    SEPARATOR=";"
+else
+    SEPARATOR=":"
+fi
+
+CLASSPATH=${YKSTORAGE_HOME}/bin
+CLASSPATH=$CLASSPATH$SEPARATOR${YKSTORAGE_HOME}/lib/slf4j-api-1.7.2.jar
+CLASSPATH=$CLASSPATH$SEPARATOR${YKSTORAGE_HOME}/lib/logback-classic-1.0.9.jar
+CLASSPATH=$CLASSPATH$SEPARATOR${YKSTORAGE_HOME}/lib/logback-core-1.0.9.jar
+
+java -cp $CLASSPATH jp.ac.titech.cs.de.ykstorage.frontend.FrontEnd
