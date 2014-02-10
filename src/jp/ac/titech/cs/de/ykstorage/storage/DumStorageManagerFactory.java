d0e84ad319a30ca7c852a42fc83b1e9b615ede2f
hikida satoshi <hikida@de.cs.titech.ac.jp>
2014-02-10 14:40:36 +0900

    add Dum server manager factory class

---
+++ src/jp/ac/titech/cs/de/ykstorage/storage/DumStorageManagerFactory.java
@@ -0,0 +1,34 @@
+package jp.ac.titech.cs.de.ykstorage.storage;
+
+import jp.ac.titech.cs.de.ykstorage.service.Parameter;
+import jp.ac.titech.cs.de.ykstorage.storage.buffer.IBufferManager;
+import jp.ac.titech.cs.de.ykstorage.storage.cachedisk.ICacheDiskManager;
+import jp.ac.titech.cs.de.ykstorage.storage.datadisk.IDataDiskManager;
+
+
+public class DumStorageManagerFactory extends StorageManagerFactory {
+
+    @Override
+    protected IBufferManager createBufferManager() {
+        return null;
+    }
+
+    @Override
+    protected ICacheDiskManager createCacheDiskManager() {
+        return null;
+    }
+
+    @Override
+    protected IDataDiskManager createDataDiskManager() {
+        return null;
+    }
+
+    @Override
+    protected StorageManager createStorageManager(
+            IBufferManager bufferManager,
+            ICacheDiskManager cacheDiskManager,
+            IDataDiskManager dataDiskManager,
+            Parameter parameter) {
+        return new DumStorageManager(null,null,null, parameter);
+    }
+}
