74e63986a36327f8b982034f10cf82d3ec56c5e6
hikida satoshi <hikida@de.cs.titech.ac.jp>
2014-02-28 15:42:47 +0900

    update YKStorageTestSuite

--- test/YKStorageTestSuite.java
+++ test/YKStorageTestSuite.java
@@ -3,9 +3,20 @@ import org.junit.runners.Suite;
 import test.jp.ac.titech.cs.de.ykstorage.frontend.FrontEndTest;
 import test.jp.ac.titech.cs.de.ykstorage.storage.BlockTest;
 import test.jp.ac.titech.cs.de.ykstorage.storage.buffer.BufferManagerTest;
+import test.jp.ac.titech.cs.de.ykstorage.storage.buffer.BufferRegionTest;
 import test.jp.ac.titech.cs.de.ykstorage.storage.buffer.LRUBufferTest;
 import test.jp.ac.titech.cs.de.ykstorage.storage.datadisk.MAIDDataDiskManagerTest;
 import test.jp.ac.titech.cs.de.ykstorage.storage.datadisk.NormalDataDiskManagerTest;
+
+/**
+ * Note1:
+ *     You should have teen run the StorageManager for FrontEnt test.
+ *
+ * Note2:
+ *     You should run the MAIDStorageManagerTest independently.
+ *     It need to override some final configuration of Parameter
+ *     to test.
+ */
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -15,6 +26,7 @@ import test.jp.ac.titech.cs.de.ykstorage.storage.datadisk.NormalDataDiskManagerT
         NormalDataDiskManagerTest.class,
         MAIDDataDiskManagerTest.class,
         BlockTest.class,
+        BufferRegionTest.class,
 })
 
 public class YKStorageTestSuite {
