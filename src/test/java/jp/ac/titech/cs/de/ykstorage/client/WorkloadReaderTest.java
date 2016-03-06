6a249aaf16f5fe8c6247e699bf57d6061dc6a296
Satoshi Hikida <s.hikida@hotmail.co.jp>
2016-03-06 16:19:15 +0900

    dynamic loading test fixture of WorkloadReaderTest

--- src/test/java/jp/ac/titech/cs/de/ykstorage/client/WorkloadReaderTest.java
+++ src/test/java/jp/ac/titech/cs/de/ykstorage/client/WorkloadReaderTest.java
@@ -6,16 +6,22 @@ import jp.ac.titech.cs.de.ykstorage.cli.WorkloadReader;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.InputStream;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 
 public class WorkloadReaderTest {
+
+    private static final String TEST_DATA = "WorkloadReaderTest.csv";
+
     private WorkloadReader workload;
 
     @Before
     public void setUp() {
-        workload = new WorkloadReader("./test/test/jp/ac/titech/cs/de/ykstorage/client/WorkloadReaderTest.csv");
+        String fixturePath = this.getClass().getPackage().getName().replace('.','/') + "/" + TEST_DATA;
+        workload = new WorkloadReader(this.getClass().getClassLoader().getResource(fixturePath).getPath());
     }
 
     @Test
@@ -36,18 +42,22 @@ public class WorkloadReaderTest {
     @Test
     public void getRequestTypeTest() {
         byte[] request = workload.getRequest().getRequest();
+        // assert get(read) request type
         assertThat(request[0], is((byte) 0x00));
         assertThat(request[1], is((byte) 0x10));
 
         request = workload.getRequest().getRequest();
+        // assert put(write) request type
         assertThat(request[0], is((byte) 0x00));
         assertThat(request[1], is((byte) 0x01));
 
         request = workload.getRequest().getRequest();
+        // assert get(read) request type
         assertThat(request[0], is((byte) 0x00));
         assertThat(request[1], is((byte) 0x10));
 
         request = workload.getRequest().getRequest();
+        // assert put(write) request type
         assertThat(request[0], is((byte) 0x00));
         assertThat(request[1], is((byte) 0x01));
     }
